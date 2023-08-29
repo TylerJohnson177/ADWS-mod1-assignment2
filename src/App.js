e77f9c855c7744db34497062322b89ed274d4a4c
TylerJohnson77 <tjjohnson120@my.wctc.edu>
2023-08-29 17:39:17 -0500

    Added a total count for each country

--- src/App.js
+++ src/App.js
@@ -5,38 +5,35 @@ import Country from './components/Country';
 class App extends Component {
   state = {
     countries:[
-      {id: 1, name: 'United States', goldMedalCount: 0 },
-      {id: 2, name: 'Canada', goldMedalCount: 0},
-      {id: 3, name: 'United Kingdom', goldMedalCount: 0 },
+      {id: 1, name: 'United States', totalMedalCount: 0 },
+      {id: 2, name: 'Canada', totalMedalCount: 0},
+      {id: 3, name: 'United Kingdom', totalMedalCount: 0 },
     ]
   }
-  handleIncrement = (countryId) => {
-    const country = this.state.countries.filter(c => c.id === countryId)[0];
-    country.goldMedalCount++
-    this.setState({country:country});
-  }
-  //Subtracts the number of medals
-  handleDecrement = (countryId, goldMedalCount) => {
-    const country = this.state.countries.filter(c => c.id === countryId)[0];
-
-    if(goldMedalCount > 0){
-      country.goldMedalCount--
-    }
-
-    this.setState({country:country});
-  }
   
+  handleUpdateTotal = (count, countryName) => {
+    const country = this.state.countries.filter(c => c.name === countryName)[0];
+    country.totalMedalCount = count;
+    this.setState({country:country});
+  }
+  getGrandTotal(){
+    let countriesIterable = this.state.countries.map((x) => x);
+      return countriesIterable.reduce((a, b) => a + b.totalMedalCount, 0);
+  }
+
   render() {
     return (
       <div className="App">
+        <header><h3>Total Medal Count: {this.getGrandTotal()}</h3></header>
+        <hr></hr>
         {this.state.countries.map(country =>
-        <Country
+        <Country 
+        updateTotal= { this.handleUpdateTotal }
         key = { country.id }
         id = { country.id }
         name = { country.name }
-        goldMedalCount = {country.goldMedalCount}
-        onIncrement = { this.handleIncrement }
-        onDecrement = { this.handleDecrement }/>
+        totalMedalCount = {country.totalMedalCount}
+        />
           )}
       </div>
     );
